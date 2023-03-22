df1044ea6c27c4f254a50862dfbf5a81920e00b1
Sabrina <Sabrina@telemessage.com>
2023-03-22 10:35:17 +0200

    SideBar

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -1,42 +1,42 @@
 import React from 'react';
 import './SideBar.css';
-// import HouseIcon from '@mui/icons-material/House';
-// import CalendarIcon from '@mui/icons-material/CalendarMonth';
-// import ProioritiserIcon from '@mui/icons-material/EventAvailableSharp';
-// import TeamIcon from '@mui/icons-material/Groups2Sharp';
-// import SettingsIcon from '@mui/icons-material/SettingsSharp';
-// import LogoutIcon from '@mui/icons-material/LogoutSharp';
+import HouseIcon from '@mui/icons-material/House';
+import CalendarIcon from '@mui/icons-material/CalendarMonth';
+import ProioritiserIcon from '@mui/icons-material/EventAvailableSharp';
+import TeamIcon from '@mui/icons-material/Groups2Sharp';
+import SettingsIcon from '@mui/icons-material/SettingsSharp';
+import LogoutIcon from '@mui/icons-material/LogoutSharp';
 
 function SideBar(): JSX.Element {
     return (
         <div className="Menu">
             <div style={{marginTop: '10px' , display:'flex',marginLeft: '7px'}}>
                 <div style={{display:'flex', alignItems:'center'}}>
-                    {/* <HouseIcon/> */}
+                    <HouseIcon/>
                     <h6>Home</h6>
                 </div>
             </div>
             <div style={{marginTop: '-30px' , display:'flex',marginLeft: '7px'}}>
                 <div style={{display:'flex', alignItems:'center'}}>
-                    {/* <CalendarIcon/> */}
+                    <CalendarIcon/>
                     <h6>Calendar</h6>
                 </div>
             </div>
             <div style={{marginTop: '-30px' , display:'flex',marginLeft: '7px'}}>
                 <div style={{display:'flex', alignItems:'center'}}>
-                    {/* <ProioritiserIcon/> */}
+                    <ProioritiserIcon/>
                     <h6>Proioritiser</h6>
                 </div>
             </div>
             <div style={{marginTop: '-30px' , display:'flex',marginLeft: '7px'}}>
                 <div style={{display:'flex', alignItems:'center'}}>
-                    {/* <TeamIcon/> */}
+                    <TeamIcon/>
                     <h6>Team</h6>
                 </div>
             </div>
             <div style={{marginTop: '-30px',display:'flex', marginLeft: '7px'}}>
                 <div style={{display:'flex', alignItems:'center'}}>
-                    {/* <LogoutIcon/> */}
+                    <LogoutIcon/>
                     <h6>Logout</h6>
                 </div>
             </div>
