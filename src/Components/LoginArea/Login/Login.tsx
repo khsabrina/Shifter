70200aa4bb75dc3f06903d43c86a1cb965061836
Barak Shachar <barakon111@gmail.com>
2023-03-22 12:32:09 +0200

    a

---
+++ src/Components/LoginArea/Login/Login.tsx
@@ -0,0 +1,12 @@
+import Logo from "../../LayoutArea/Logo/Logo";
+import "./Login.css";
+
+function Login(): JSX.Element {
+    return (
+        <div className="Login">
+            <Logo />
+        </div>
+    );
+}
+
+export default Login;
