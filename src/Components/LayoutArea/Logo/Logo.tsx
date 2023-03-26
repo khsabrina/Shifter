9450ca481447117f264c84c73d84b9dc74c273d9
Barak Shachar <barakon111@gmail.com>
2023-03-26 09:01:38 +0300

    .

--- src/Components/LayoutArea/Logo/Logo.tsx
+++ src/Components/LayoutArea/Logo/Logo.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
 import logo from './Logo.png'
-import { Link } from 'react-router-dom'
 
 const Logo = () => {
   return (
     <div>
-      <img className='Logo' src={logo} />
+      <img className='Logo' src={logo} alt="shifter logo" />
     </div>
   )
 }
--- src/Components/LoginArea/Input/Input.tsx
+++ src/Components/LoginArea/Input/Input.tsx
@@ -1,14 +1,49 @@
 import "./Input.css";
+import { TextField, Button, ButtonGroup } from "@mui/material";
+import { Login } from "@mui/icons-material";
+import { useForm } from "react-hook-form";
 
-
+interface LoginForm {
+    username: string;
+    password: string;
+}
 function Input(): JSX.Element {
-
-
-
-
+    // const [userName, setUserName] = React.useState<string>('');
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<LoginForm>();
+    const onSubmit = (data: LoginForm) => { console.log(data); }
     return (
-        <div className="Input">
-            <input name="email"></input>
+        <div className="Input Box">
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <TextField
+                    label="Username"
+                    // get and set the useState ^
+                    // value={userName}
+                    // onChange={(e) => setUserName(e.target.value)}
+                    variant="outlined"
+                    className="TextBox"
+                    {...register("username")}
+                />
+
+                <TextField
+                    label="Password"
+                    variant="outlined"
+                    className="TextBox"
+                    type='password'
+                    {...register("password", { required: true, minLength: 2, maxLength: { message: 'barak noob at react', value: 20 } })}
+                />
+
+                <ButtonGroup variant="contained" fullWidth>
+                    <Button
+                        type="submit"
+                        color="primary"
+                        startIcon={<Login />}
+                    >
+                        Login
+                    </Button>
+                </ButtonGroup>
+            </form>
         </div>
     );
 }
