f60fb4dd52c4a51a6e9c364a5cb743eebfcdcdcf
Barak Shachar <barakon111@gmail.com>
2023-04-05 16:52:26 +0300

    s

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Components/LayoutArea/Layout/Layout";
 import Login from "./Components/LoginArea/Login/Login";
 import Home from "./Components/Pages/Home/Home"
 import Calendar from "./Components/Pages/Calendar/Calendar"
@@ -13,7 +12,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/calendar" element={<Calendar />} />
         <Route path="/prioritizer" element={<Prioritizer />} />
